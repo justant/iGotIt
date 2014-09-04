584fbcff922e5ebf99ab5baa3a00dde1a5c6ea71
justant <justant@naver.com>
2014-09-04 17:30:51 +0900

    140904 17:30

--- iGotIt/src/database/DatabaseManager.java
+++ iGotIt/src/database/DatabaseManager.java
@@ -1,5 +1,118 @@
 package database;
 
+import java.io.IOException;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
 public class DatabaseManager {
+	private static final String TAG = "DataBaseManager";
+	private static final String DATABASE_NAME = "iGotIt.sqlite";
+	private static final int DATABASE_VERSION = 1;
+
+	private SQLiteHelper mDBHelper;
+	private SQLiteDatabase mDB;
+	private final Context mContext;
+
+	public DatabaseManager(Context context) {
+		this.mContext = context;
+	}
+	// DB를 만들거나 업그레이드 할 때 사용한다.
+	private static class SQLiteHelper extends SQLiteOpenHelper {
+		SQLiteHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			// 이미 DB가 만들어 졌으므로 필요 없다
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			Log.v(TAG, "Upgrading database from version " + oldVersion + " to "
+					+ newVersion + ", which will destroy all old data");
+			// 이미 DB를 업데이트할 일이 없다.
+			onCreate(db);
+		}
+	}
+	
+	public DatabaseManager open() throws SQLException {
+		mDBHelper = new SQLiteHelper(mContext);
+		mDB = mDBHelper.getWritableDatabase();
+		// 최적화를 위해 동기화 off
+		mDB.execSQL("PRAGMA synchronous = OFF");
+
+		Log.v(TAG, "database open");
+		return this;
+	}
+
+	public void close() {
+		mDBHelper.close();
+	}
+	
+	// Transaction 시작. 
+	public void setBeginTransaction(){
+		mDB.beginTransaction();
+	}
+	
+	// Transaction 성공.
+	public void setTransactionSuccessful(){
+		mDB.setTransactionSuccessful();
+	}
+	
+	// Transaction 종료.
+	public void setEndTransaction(){
+		mDB.endTransaction();
+	}
+	
+	// insert 구현
+	// delete 구현
+	
+	// update 구현
+	public void update(String tableName, String[] fields, String[] values) {
+		// 필드와 값의 갯수가 같지 않으면 오류 발생
+		if(fields.length != values.length){
+			Log.v(TAG, "update : fields.length is not equal to values.length");
+			return;
+		}
+		
+		int i;
+		String sql = "UPDATE " + tableName + " SET ";
+		for(i = 0; i<fields.length - 1; i++){
+			sql += fields[i] + " = '" + values[i] + "', ";
+		}
+		sql += fields[i] + " = '" + values[i] + "'";
+		mDB.execSQL(sql);
+		
+		Log.v(TAG, "update sql = " + sql);
+	}
+	
+	public void update(String tableName, String[] fields, String[] values, String where) {
+	
+	}
+		
+		
+		
+//		if (tableName.equals("word") && fieldNumber == 4) {
+//			/* fields[0] = word_ID, fields[1] = knowlevel, fields[2] = knowtime, fields[3] = knowfile */
+//			Log.v(TAG, "SQL(UPDATE) TABLE NAME = " + tableName + "fields[0]="+fields[0]+", fields[1]="+fields[1]+", fields[2]="+fields[2]+", fields[3]="+fields[3]);
+//			mDB.execSQL("UPDATE " + tableName + " SET " + "knowlevel='" + Integer.parseInt(fields[1])
+//						+ "', knowtime='" + fields[2] + "', knowfile='" + fields[3] + "' " + "WHERE word_ID=" + fields[0]);
+//		
+//		}	
+	 
+	
+	// fetch
+	
+	// tableName의 모든 DB를 fetch한다.
+	public Cursor fetchAll(String tableName) {
+		Log.v(TAG, " fetchAll data , tableName : " + tableName);
+		return mDB.rawQuery("SELECT * FROM " + tableName, null);
+	}
 
 }
--- iGotIt/src/singleton/Utility.java
+++ iGotIt/src/singleton/Utility.java
@@ -6,33 +6,70 @@ import com.facebook.widget.ProfilePictureView;
 public class Utility {
 	private static final String TAG = "Singleton Utility";
 	private static Utility utility = new Utility();
-	
-	private Utility(){
-	
-	}
-	
+	private Utility(){}
 	public static Utility getInstance(){
 		Log.v(TAG, "Singleton Utility getInstance()");
 		return utility;
 	}
 	
-	private String facebookId = null;
-	private String facebookName = null;
-	private ProfilePictureView profilePictureView;
-	public String getFacebookId() {
-		Log.v(TAG, "getFacebookId() = " + facebookId);
-		return facebookId;
+
+	// 페이스북 사용자 ID
+	private String facebook_id;
+	// 페이스북 사용자 이름
+	private String facebook_name;
+	// 페이스북 사용자 메일
+	private String facebook_email;
+	// 페이스북 프로필 사진
+	private ProfilePictureView facebook_profilePicture;
+	// tts 음높이
+	private String preference_ttsPitch;
+	// tts 빠르기
+	private String preference_ttsRate;
+	// 이미지학습 타이머 시간
+	private String preference_timeLimit;
+
+	public String getFacebook_id() {
+		return facebook_id;
 	}
-	public void setFacebookId(String facebookId) {
-		Log.v(TAG, "setFacebookId(String facebookId) = " + facebookId);
-		this.facebookId = facebookId;
+	public void setFacebook_id(String facebook_id) {
+		this.facebook_id = facebook_id;
 	}
-	public String getFacebookName() {
-		Log.v(TAG, "getFacebookName() = " + facebookName);
-		return facebookName;
+	public String getFacebook_name() {
+		return facebook_name;
 	}
-	public void setFacebookName(String facebookName) {
-		Log.v(TAG, "setFacebookName(String facebookName) = " + facebookName);
-		this.facebookName = facebookName;
+	public void setFacebook_name(String facebook_name) {
+		this.facebook_name = facebook_name;
 	}
+	public String getFacebook_email() {
+		return facebook_email;
+	}
+	public void setFacebook_email(String facebook_email) {
+		this.facebook_email = facebook_email;
+	}
+	public ProfilePictureView getFacebook_profilePicture() {
+		return facebook_profilePicture;
+	}
+	public void setFacebook_profilePicture(
+			ProfilePictureView facebook_profilePicture) {
+		this.facebook_profilePicture = facebook_profilePicture;
+	}
+	public String getPreference_ttsPitch() {
+		return preference_ttsPitch;
+	}
+	public void setPreference_ttsPitch(String preference_ttsPitch) {
+		this.preference_ttsPitch = preference_ttsPitch;
+	}
+	public String getPreference_ttsRate() {
+		return preference_ttsRate;
+	}
+	public void setPreference_ttsRate(String preference_ttsRate) {
+		this.preference_ttsRate = preference_ttsRate;
+	}
+	public String getPreference_timeLimit() {
+		return preference_timeLimit;
+	}
+	public void setPreference_timeLimit(String preference_timeLimit) {
+		this.preference_timeLimit = preference_timeLimit;
+	}
+	
 }
